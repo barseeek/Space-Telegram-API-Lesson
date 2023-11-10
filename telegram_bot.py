9f5ac290b633154a9e8b41d9aea7b9f9cbd016bc
Nikita <barskii.no@outlook.com>
2023-11-10 12:48:35 +0300

    Publish text to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
-    print(bot.get_me())
+    bot.send_message(text="First message", chat_id="@space_imagess")
 
 if __name__ == "__main__":
     main()
