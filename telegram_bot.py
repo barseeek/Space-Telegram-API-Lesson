f2f29fa8502ddbada6f3dbfa1632eb5f2f9e6a66
Nikita <barskii.no@outlook.com>
2023-11-10 12:57:28 +0300

    Publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
+    chat_id = "@space_imagess"
     bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
-    bot.send_message(text="First message", chat_id="@space_imagess")
+    bot.send_photo(chat_id=chat_id, photo=open('images/apod_2023-11-10.jpg', 'rb'))
 
 if __name__ == "__main__":
     main()
